6858ce1b8f153d34416ad29ef4d7774d40cafc2e
barbaragabrielasp <barbaragabrielasp@gmail.com>
2024-10-13 19:23:47 -0300

    added first obj

--- src/main/java/com/example/net/salamandemon/testmod/TestMod.java
+++ src/main/java/com/example/net/salamandemon/testmod/TestMod.java
@@ -1,5 +1,7 @@
 package com.example.net.salamandemon.testmod;
 
+import com.example.net.salamandemon.testmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +36,8 @@ public class TestMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -46,7 +50,9 @@ public class TestMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.LAURA);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/example/net/salamandemon/testmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.example.net.salamandemon.testmod.item;
+
+import com.example.net.salamandemon.testmod.TestMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TestMod.MOD_ID);
+
+    public static final DeferredItem<Item> LAURA = ITEMS.register("laura",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
